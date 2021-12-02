5442877b8eb476437252e5051661b7ab0b82383e
Gleb94 <58596253+Gleb94@users.noreply.github.com>
2021-12-02 17:09:39 +0300

    add

---
+++ Angular/binding/src/app/test/test.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: '<div>Inline tmplate</div>',
+  styleUrls: []
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+ss
