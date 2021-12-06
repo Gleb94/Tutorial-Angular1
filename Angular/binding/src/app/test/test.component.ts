150d5775e92fefafdb471c285163bcf562460df1
Gleb94 <58596253+Gleb94@users.noreply.github.com>
2021-12-06 17:23:52 +0300

    add

--- Angular/binding/src/app/test/test.component.ts
+++ Angular/binding/src/app/test/test.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: '<div>Inline tmplate</div>',
+  template: '<div>Welcome Vashwas</div>',
   styleUrls: []
 })
 export class TestComponent implements OnInit {
