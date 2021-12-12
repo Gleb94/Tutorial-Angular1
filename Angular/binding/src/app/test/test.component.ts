5005aa12a65c6d531547e80200826d37635a78c9
Gleb94 <58596253+Gleb94@users.noreply.github.com>
2021-12-12 21:15:45 +0300

    add

--- Angular/binding/src/app/test/test.component.ts
+++ Angular/binding/src/app/test/test.component.ts
@@ -2,14 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: '<div>Welcome Vashwas</div>',
+  template: '
+  <h2>
+    Welcome {{name}}
+  </h2>
+  <h2 [style.color] ="hasError? 'red' : 'green'">Style Binding</h2>
+  <h2 [style.color] = "hightlightColor">Style Binding2</h2>
+  <h2 [ngStyle] = "titleSryles">Style Binding3</h2>',',
   styleUrls: []
 })
 export class TestComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public name = "codevolution";
+  public hasError = false;
+  public isSpeciak = true;
+  public hightlightColor = "orenge";
+  public titleSryles = {
+    color: "blue",
+    frontStyle: "Italic"
   }
+  constructor(){}
+  
+  ngOnInit(){
 
+  }
 }
